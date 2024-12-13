7c6369439fff2bf3e0ef58adcb86dfd00f1ea6ae
OmkarM <omimalpote9130@gmail.com>
2024-12-14 00:33:47 +0530

    Latest Code to the server

--- library_management/settings/local.py
+++ library_management/settings/local.py
@@ -15,7 +15,7 @@ DATABASES = {
         "HOST": "127.0.0.1",
         "PORT": "3306",
         "USER": "root",
-        "PASSWORD": "password", # password
+        "PASSWORD": "S3cure#Passw0rd!", # password
     }
 }
 
